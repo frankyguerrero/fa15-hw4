aa4d2bfec8e2300717c12de71161404d6555f4e5
frankyguerrero <rocio_guerrero@berkeley.edu>
2015-10-09 23:18:55 -0700

    IM DONE

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,23 @@
+class DogsController < ApplicationController
+	def index
+    	@dogs = Dog.all
+  	end
+
+  	def new
+    	@dog = Dog.new
+  	end
+
+  	def create
+    	Dog.create dog_params
+    	redirect_to :dogs
+  	end
+
+  	def dog_params
+    	params.require(:dog).permit(:name, :age)
+  	end
+
+  	def show
+    	@dog = Dog.find(params[:id])
+  	end
+
+end
